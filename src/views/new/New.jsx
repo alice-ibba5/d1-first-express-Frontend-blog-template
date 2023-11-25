1a0879ea8d3f6fed0b90f19e4dcb2d2e47c210c7
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-11-26 00:34:02 +0100

    up

--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -3,7 +3,7 @@ import { Button, Container, Form, Spinner } from "react-bootstrap";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./styles.css";
-import { toast } from "react-toastify";
+//import { toast } from "react-toastify";
 
 const NewBlogPost = (props) => {
   const [text, setText] = useState("");
@@ -32,11 +32,11 @@ const NewBlogPost = (props) => {
       author: {
         _id: idAuthor,
       },
-      createdAt: createdAt,      
+      createdAt: createdAt,
     };
 
     try {
-      fetch("http://localhost:3000/api/blogPosts/", {
+      fetch("http://localhost:3000/api/blogPosts", {
         headers: {
           "Content-Type": "application/json",
         },
@@ -46,9 +46,9 @@ const NewBlogPost = (props) => {
       })
         .then(function (response) {
           if (response.ok) {
-            toast.success("Comment saved successfully!", {
-              position: toast.POSITION.BOTTOM_RIGHT,
-            });
+            //toast.success("Comment saved successfully!", {
+            // position: toast.POSITION.BOTTOM_RIGHT,
+            //});
           } else {
             // toast.error("Something went wrong!", {
             //   position: toast.POSITION.TOP_LEFT,
@@ -68,16 +68,42 @@ const NewBlogPost = (props) => {
 
 
 
-  return (
+  return loading ? (
+    <div className="d-flex mt-5">
+      <Spinner animation="border" variant="primary" className="mx-auto" />
+    </div>
+  ) : (
     <Container className="new-blog-container">
-      <Form className="mt-5">
+      <Form className="mt-5" onSubmit={handleSubmit}>
         <Form.Group controlId="blog-form" className="mt-3">
           <Form.Label>Titolo</Form.Label>
-          <Form.Control size="lg" placeholder="Title" />
+          <Form.Control
+            size="lg"
+            placeholder="Title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)} />
         </Form.Group>
+
+        <Form.Group controlId="blog-form" className="mt-3">
+          <Form.Label>ID Authors</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="2348762397429"
+            required
+            value={idAuthor}
+            onChange={(e) => setIdAuthor(e.target.value)}
+          />
+        </Form.Group>
+
         <Form.Group controlId="blog-category" className="mt-3">
           <Form.Label>Categoria</Form.Label>
-          <Form.Control size="lg" as="select">
+          <Form.Control
+            size="lg"
+            as="select"
+            required
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
             <option>Categoria 1</option>
             <option>Categoria 2</option>
             <option>Categoria 3</option>
@@ -85,6 +111,18 @@ const NewBlogPost = (props) => {
             <option>Categoria 5</option>
           </Form.Control>
         </Form.Group>
+
+        <Form.Group className="mt-3">
+          <Form.Label>Tempo di lettura</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="3 minuti"
+            required
+            value={readTime}
+            onChange={(e) => setReadTime(e.target.value)}
+          />
+        </Form.Group>
+
         <Form.Group controlId="blog-content" className="mt-3">
           <Form.Label>Contenuto Blog</Form.Label>
           <ReactQuill value={text} onChange={handleChange} className="new-blog-content" />
